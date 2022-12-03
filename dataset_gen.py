534731e0c8c8a1b61706b5f366d627bc8181d6d1
yindaheng98 <yindaheng98@gmail.com>
2022-12-03 23:03:19 +0800

    cut video cmd

--- dataset_gen.py
+++ dataset_gen.py
@@ -23,12 +23,17 @@ for name in os.listdir(root):
     print(rate, frame_num, clip_num)
 
     valid_idx = list(range(0, clip_num, train_rate + 1))
+    train_idx = 0
     for idx in range(clip_num):
         frame_start = idx * clip_length
         frame_end = frame_start+clip_length
         clip_name = os.path.splitext(name)[0]+f"-{frame_start}_{frame_end}" + os.path.splitext(name)[-1]
         if idx in valid_idx:
-            print("valid", clip_name)
+            clip_path = os.path.join(valid_root, clip_name)
+            print("valid", clip_name, clip_path)
+            train_idx += 1
         else:
-            print("train", clip_name)
-        
+            clip_path = os.path.join(train_root, str(train_idx), clip_name)
+            print("train", clip_name, clip_path)
+        cmd = [ffmpeg, '-i', path, '-vf', f'select=between(n\,{frame_start}\,{frame_end})', '-y', 'copy', clip_path]
+        print(cmd)
